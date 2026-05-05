3234d3b0dc4b32be7c5b516a88ff8a96613e4a02
Shash <sg276278@gmail.com>
2026-05-05 07:54:22 +0530

    fix CSS styling for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     TanStackRouterVite(),
     react(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
